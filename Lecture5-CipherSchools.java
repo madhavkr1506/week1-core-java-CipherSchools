c7b966f58559cc4cf06a8018625d0e67341cc1fe
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-03-14 15:16:47 +0530

    Operators & Operands

---
+++ Lecture5-CipherSchools.java
@@ -0,0 +1,30 @@
+// Operators and Operands
+public class primitiveDT{
+  public static void main(String[] args){
+    int x = 5;
+    int y = 7;
+    System.out.println("X is "+ x);
+    System.out.println("Y is "+ y);
+
+    byte b = 10;
+    System.out.println("B is "+ b);
+    byte b2 = (byte)129;
+    System.out.println("B is "+ b2);
+    byte b3 = (byte)128;
+    System.out.println("B is "+ b3);
+    byte b3 = (byte)256;
+    System.out.println("B is "+ b3);
+
+    int i = b;
+    System.out.println("I is "+ i);
+
+    float f = (float)5.5;
+    float f2 = 5.5f;
+    double d = 10.5;
+    char ch = 'a';
+    System.out.println(ch);
+
+    int z = x +y;
+
+     int n = 
+}
