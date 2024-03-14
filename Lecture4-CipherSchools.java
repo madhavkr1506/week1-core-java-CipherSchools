aa07a974c78ed2dee1939e3132a5859987403ef8
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-03-14 15:16:08 +0530

    Primitive Data Types & Type Casting

---
+++ Lecture4-CipherSchools.java
@@ -0,0 +1,20 @@
+// primitive data types
+public class primitiveDT{
+  public static void main(String[] args){
+    int x = 5;
+    int y = 7;
+    System.out.println("X is "+ x);
+    System.out.println("Y is "+ y);
+
+    byte b = 10;
+    System.out.println("B is "+ b);
+    byte b2 = (byte)129;
+    System.out.println("B is "+ b2);
+    byte b3 = (byte)128;
+    System.out.println("B is "+ b3);
+    byte b3 = (byte)256;
+    System.out.println("B is "+ b3);
+
+    int i = b;
+    System.out.println("I is "+ i);
+}
